d4d764d0505f60a35a1d682293b5e905f8d54ea6
Lanny Heidbreder <lanny@pleth.com>
2012-10-16 07:52:12 -0500

    Clean up header &c of page.tpl.php
    Signed-off-by: Lanny Heidbreder <lanny@pleth.com>

--- copy_me/templates/page.tpl.php
+++ copy_me/templates/page.tpl.php
@@ -79,30 +79,14 @@
 		<?php endif; ?>
 
 		<div id="header">
-
-			<?php if ($logo): ?>
-				<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-			<?php endif; ?>
-
-			<?php if ($site_name || $site_slogan): ?>
-				<hgroup>
-					<div id="name-and-slogan">
-						<?php if ($site_name): ?>
-							<h1 id="site-name">
-								<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-							</h1>
-						<?php endif; ?>
-
-						<?php if ($site_slogan): ?>
-							<h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-						<?php endif; ?>
-					</div> <!-- #name-and-slogan -->
-				</hgroup>
-			<?php endif; ?>
+			<h1>
+				<a href="<?php print $front_page; ?>" rel="home"><span><?php print $site_name; ?></span></a>
+			</h1>
 
 			<?php print render($page['header']); ?>
-
 		</div> <!-- #header -->
+
+		<?php print render($page['navigation']); ?>
 
 		<div id="main-wrapper">
 			<div id="main" <?php if ($main_menu || $page['navigation']) { print 'class="with-navigation"'; } ?>>
