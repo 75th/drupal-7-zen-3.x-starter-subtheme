58a9cab73dfc096f3efa933269459dd270c63fa7
Lanny Heidbreder <lheidbreder@stoneward.com>
2015-09-29 10:36:29 -0500

    Imported Zen 5.x tabs, minus the SMACSS terribleness

--- template.php
+++ template.php
@@ -88,3 +88,70 @@ function copy_me_preprocess_location(&$variables) {
 		$variables['map_link'] = location_map_link($variables['location'], '');
 	}
 }
+
+
+/**
+ * Returns HTML for primary and secondary local tasks.
+ *
+ * @ingroup themeable
+ */
+function copy_me_menu_local_tasks(&$variables) {
+	$output = '';
+
+	// Add theme hook suggestions for tab type.
+	foreach (array('primary', 'secondary') as $type) {
+		if (!empty($variables[$type])) {
+			foreach (array_keys($variables[$type]) as $key) {
+				if (isset($variables[$type][$key]['#theme']) && ($variables[$type][$key]['#theme'] == 'menu_local_task' || is_array($variables[$type][$key]['#theme']) && in_array('menu_local_task', $variables[$type][$key]['#theme']))) {
+					$variables[$type][$key]['#theme'] = array('menu_local_task__' . $type, 'menu_local_task');
+				}
+			}
+		}
+	}
+
+	if (!empty($variables['primary'])) {
+		$variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+		$variables['primary']['#prefix'] .= '<ul class="tabs primary">';
+		$variables['primary']['#suffix'] = '</ul>';
+		$output .= drupal_render($variables['primary']);
+	}
+	if (!empty($variables['secondary'])) {
+		$variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+		$variables['secondary']['#prefix'] .= '<ul class="tabs secondary">';
+		$variables['secondary']['#suffix'] = '</ul>';
+		$output .= drupal_render($variables['secondary']);
+	}
+
+	return $output;
+}
+
+/**
+ * Returns HTML for a single local task link.
+ *
+ * @ingroup themeable
+ */
+function copy_me_menu_local_task($variables) {
+	$class = '';
+
+	$link = $variables['element']['#link'];
+	$link_text = $link['title'];
+
+	if (!empty($variables['element']['#active'])) {
+		// Add text to indicate active tab for non-visual users.
+		$active = ' <span class="element-invisible">' . t('(active tab)') . '</span>';
+
+		// If the link does not contain HTML already, check_plain() it now.
+		// After we set 'html'=TRUE the link will not be sanitized by l().
+		if (empty($link['localized_options']['html'])) {
+			$link['title'] = check_plain($link['title']);
+		}
+
+		$link['localized_options']['html'] = TRUE;
+		$link_text = t('!local-task-title!active', array('!local-task-title' => $link['title'], '!active' => $active));
+
+		$class = 'active';
+
+	}
+
+	return '<li' . ($class ? ' class="' . $class . '"' : '') . '>' . l($link_text, $link['href'], $link['localized_options']) . "</li>\n";
+}
