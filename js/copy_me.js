0e78ff72fdd5049ad968bef62de53d7d43497c8d
Lanny Heidbreder <lheidbreder@stoneward.com>
2015-09-29 10:35:17 -0500

    Freed ourselves from the bondage of loading multiple jQuery libraries on one site

--- js/copy_me.js
+++ js/copy_me.js
@@ -1,29 +1,9 @@
-$ = jQuery.noConflict(true);
+(function($) {
 
-$(document).ready(function() {
+	$(document).ready(function() {
 
-	responder = jRespond([
-		{
-			'label': 'desktop-large',
-			'enter': 1200,
-			'exit': 100000
-		},{
-			'label': 'desktop-small',
-			'enter': 960,
-			'exit': 1199
-		},{
-			'label': 'tablet',
-			'enter': 768,
-			'exit': 959
-		},{
-			'label': 'mobile-large',
-			'enter': 500,
-			'exit': 767
-		},{
-			'label': 'mobile-small',
-			'enter': 0,
-			'exit': 499
-		}
-	]);
-	
-});
+
+	});
+
+})(jQuery);
+
