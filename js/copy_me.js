311c5304069e1053d825f3650f2e07a7f84793fc
Lanny Heidbreder <lanny@75thtrombone.com>
2017-10-04 14:18:45 -0500

    Fixed dimensions.scss reference; added fallback sans-serif typefaces; deleted mediaCheck boilerplate

--- js/copy_me.js
+++ js/copy_me.js
@@ -1,23 +1,5 @@
 (function($) {
-
 	$(document).ready(function() {
-
-		// The mediaCheck library lets you execute code when entering and/or leaving media queries.
-		//
-		// The same breakpoints used by Less are available as JavaScript variables under Drupal.settings.breakpoints.
-
-		mediaCheck({
-			media: '(max-width: ' + Drupal.settings.breakpoints.mobile.max + 'px)',
-			entry: function() {
-
-			},
-			exit: function() {
-
-			},
-			both: function() {
-
-			}
-		});
 
 	});
 })(jQuery);
